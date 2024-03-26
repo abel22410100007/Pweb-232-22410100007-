b0b5f1cc8dc4ccef2ee7b950168cc6624ccfd9aa
abel22410100007 <164967132+abel22410100007@users.noreply.github.com>
2024-03-26 08:37:48 +0700

    Add files via upload

---
+++ tugas1.php
@@ -0,0 +1,52 @@
+<html>
+    <head>
+        <title>Tugas 1</title>
+    </head>
+    <body>
+    <a href="tugas11.php" target="_self"><Button><h3>+</h3></Button></a>
+        <table border="1">
+        <caption>Produk</caption>
+            <tr>
+                <th style="width:10px" rowspan="2">kode</th>
+                <th style="width:100px"rowspan="2">nama</th>
+                <th style="width:30px" rowspan="2">satuan</th>
+                <th style="width:50px"colspan="2">harga</th>
+                <th style="width:30px"rowspan="2">Action</th>
+            </tr>
+            <tr>
+                    <td>Beli</td>
+                    <td>Jual</td>
+            </tr>
+            <tr>
+                    <td>M01</td>
+                    <td>CPU</td>
+                    <td>5pcs</td>
+                    <td>200</td>
+                    <td>350</td>
+                    <td>
+                        <a href="tugas12.php"><Button>EDIT</Button></a>
+                    </td>
+            </tr>
+            <tr>
+                    <td>M02</td>
+                    <td>RAM</td>
+                    <td>10pcs</td>
+                    <td>300</td>
+                    <td>450</td>
+                    <td>
+                        <a href="tugas12.php"><Button>EDIT</Button></a>
+                    </td>
+            </tr>
+            <tr>
+                    <td>M03</td>
+                    <td>VGA</td>
+                    <td>15pcs</td>
+                    <td>400</td>
+                    <td>550</td>
+                    <td>
+                        <a href="tugas12.php"><Button>EDIT</Button></a>
+                    </td>
+            </tr>
+        </table>
+    </body>
+</html>
---
+++ tugas11.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html>
+<head>
+</head>
+<body>
+<form action="tugas1.php" method="POST">
+  <label for="kode"> Kode: </label><br>
+  <input type="text" id="kode" name="kode"><br>
+  <label for="nama"> Nama: </label><br>
+  <input type="text" id="nama" name="nama"><br>
+  <label for="satuan"> Satuan: </label><br>
+  <input type="number" id="satuan" name="satuan"><br>
+  <label for="beli"> Beli: </label><br>
+  <input type="number" id="beli" name="beli"><br>
+  <label for="jual"> Jual: </label><br>
+  <input type="number" id="jual" name="jual"><br><br>
+  <input type="submit" value="Save" name="save">
+  <input type="submit" value="Cancel" name="cancel">
+</form>
+</body>
+</html>
